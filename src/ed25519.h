4cd9e80cce8023eb0768e60292fbc2aa5f8c461e
Piero V. <vogliadifarniente@gmail.com>
2024-09-14 21:09:50 +0200

    Allow incremental verifications.
    Expose the SHA512 interface to consumers, and allow to incrementally
    update the hash, to verify signatures without having the complete
    message in memory.

--- src/ed25519.h
+++ src/ed25519.h
@@ -3,6 +3,8 @@
 
 #include <stddef.h>
 
+#include "sha512.h"
+
 #if defined(_WIN32)
     #if defined(ED25519_BUILD_DLL)
         #define ED25519_DECLSPEC __declspec(dllexport)
@@ -15,6 +17,11 @@
     #define ED25519_DECLSPEC
 #endif
 
+typedef struct verify_context_ {
+  const unsigned char *signature;
+  const unsigned char *public_key;
+  sha512_context hash;
+} verify_context;
 
 #ifdef __cplusplus
 extern "C" {
@@ -30,6 +37,9 @@ int ED25519_DECLSPEC ed25519_verify(const unsigned char *signature, const unsign
 void ED25519_DECLSPEC ed25519_add_scalar(unsigned char *public_key, unsigned char *private_key, const unsigned char *scalar);
 void ED25519_DECLSPEC ed25519_key_exchange(unsigned char *shared_secret, const unsigned char *public_key, const unsigned char *private_key);
 
+void ED25519_DECLSPEC ed25519_verify_init(verify_context *ctx);
+void ED25519_DECLSPEC ed25519_verify_update(verify_context *ctx, const unsigned char *message, size_t message_len);
+int ED25519_DECLSPEC ed25519_verify_final(verify_context *ctx);
 
 #ifdef __cplusplus
 }
--- src/verify.c
+++ src/verify.c
@@ -45,31 +45,45 @@ static int consttime_equal(const unsigned char *x, const unsigned char *y) {
 }
 
 int ed25519_verify(const unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key) {
+  verify_context ctx;
+  ctx.signature = signature;
+  ctx.public_key = public_key;
+  ed25519_verify_init(&ctx);
+  ed25519_verify_update(&ctx, message, message_len);
+  return ed25519_verify_final(&ctx);
+}
+
+void ed25519_verify_init(verify_context *ctx) {
+    sha512_init(&ctx->hash);
+    sha512_update(&ctx->hash, ctx->signature, 32);
+    sha512_update(&ctx->hash, ctx->public_key, 32);
+}
+
+void ed25519_verify_update(verify_context *ctx, const unsigned char *message, size_t message_len) {
+  sha512_update(&ctx->hash, message, message_len);
+}
+
+int ed25519_verify_final(verify_context *ctx) {
     unsigned char h[64];
     unsigned char checker[32];
-    sha512_context hash;
     ge_p3 A;
     ge_p2 R;
 
-    if (signature[63] & 224) {
+    if (ctx->signature[63] & 224) {
         return 0;
     }
 
-    if (ge_frombytes_negate_vartime(&A, public_key) != 0) {
+    if (ge_frombytes_negate_vartime(&A, ctx->public_key) != 0) {
         return 0;
     }
 
-    sha512_init(&hash);
-    sha512_update(&hash, signature, 32);
-    sha512_update(&hash, public_key, 32);
-    sha512_update(&hash, message, message_len);
-    sha512_final(&hash, h);
-    
+    sha512_final(&ctx->hash, h);
+
     sc_reduce(h);
-    ge_double_scalarmult_vartime(&R, h, &A, signature + 32);
+    ge_double_scalarmult_vartime(&R, h, &A, ctx->signature + 32);
     ge_tobytes(checker, &R);
 
-    if (!consttime_equal(checker, signature)) {
+    if (!consttime_equal(checker, ctx->signature)) {
         return 0;
     }
 
